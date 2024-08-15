10b0700f55331348063f0843cc6c8b5d11498be8
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-08-15 14:06:16 +0800

    fix: .ssh
    悔しい
    
    Signed-off-by: XiaozhiSans <xiaozhisans-awa@outlook.com>

--- main.bat
+++ main.bat
@@ -4,7 +4,8 @@ set defaultName="XiaozhiSans"
 set defaultEmail="xiaozhisans-awa@outlook.com"
 
 :git_config
-copy %gitPePath%\.ssh %userprofile% /y && echo successfully config ssh || echo. && echo copy ".ssh" to %userprofile% faild && echo.
+mkdir %userprofile%\.ssh
+copy %gitPePath%\.ssh %userprofile%\.ssh /y && echo successfully config ssh || echo. && echo copy ".ssh" to %userprofile% faild && echo.
 git config --global user.name %defaultName%
 git config --global user.email %defaultEmail% && echo.
 echo pls check config:
