557af2ba7ee92b23988d9778bb7ffd2860eef717
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-08-15 15:16:32 +0800

    ✨ feat: support key => ssh-agent ~(semi-automatic)~

--- main.bat
+++ main.bat
@@ -5,7 +5,7 @@ set defaultEmail="xiaozhisans-awa@outlook.com"
 
 :git_config
 mkdir %userprofile%\.ssh
-copy %gitPePath%\.ssh %userprofile%\.ssh /y && echo successfully config ssh || echo. && echo copy ".ssh" to %userprofile% faild && echo.
+copy %gitPePath%\.ssh %userprofile%\.ssh /y && sh ssh.sh || echo. && echo copy ".ssh" to %userprofile% faild && echo.
 git config --global user.name %defaultName%
 git config --global user.email %defaultEmail% && echo.
 echo pls check config:
---
+++ ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "" && echo "pls input \"ssh-add x:/users/default/.ssh/id_rsa && exit\""
+ssh-agent bash
